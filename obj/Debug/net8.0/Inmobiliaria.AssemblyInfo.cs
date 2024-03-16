493c6819dce677c5859987f3c7caa4d02af23cf4
Mauro Corvalan <105509270+Maurocorvalan@users.noreply.github.com>
2024-03-16 17:53:19 -0300

    Mysql
    Se añade MySql al proyecto

--- obj/Debug/net8.0/Inmobiliaria.AssemblyInfo.cs
+++ obj/Debug/net8.0/Inmobiliaria.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inmobiliaria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0295b6770a8a027b4be9e2e544916efc6fce10f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inmobiliaria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inmobiliaria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
