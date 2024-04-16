6d1e749b5d6b01a7500ff2b2e57a240d83d62421
Mauro Corvalan <105509270+Maurocorvalan@users.noreply.github.com>
2024-04-16 00:27:05 -0300

    d
    d

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,28 +2,57 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-    function mostrarEditarFoto() {
+let fotoFormVisible = false;
+let datosFormVisible = false;
+let contraseñaFormVisible = false;
+
+function mostrarEditarFoto() {
+    if (fotoFormVisible) {
+        document.getElementById("editarFotoForm").style.display = "none";
+        fotoFormVisible = false;
+    } else {
         document.getElementById("editarFotoForm").style.display = "block";
         document.getElementById("editarDatosForm").style.display = "none";
         document.getElementById("cambiarContraseñaForm").style.display = "none";
+        fotoFormVisible = true;
+        datosFormVisible = false;
+        contraseñaFormVisible = false;
     }
+}
 
-    function mostrarEditarDatos() {
-        document.getElementById("editarFotoForm").style.display = "none";
+function mostrarEditarDatos() {
+    if (datosFormVisible) {
+        document.getElementById("editarDatosForm").style.display = "none";
+        datosFormVisible = false;
+    } else {
         document.getElementById("editarDatosForm").style.display = "block";
+        document.getElementById("editarFotoForm").style.display = "none";
         document.getElementById("cambiarContraseñaForm").style.display = "none";
+        datosFormVisible = true;
+        fotoFormVisible = false;
+        contraseñaFormVisible = false;
     }
+}
 
-    function mostrarCambiarContraseña() {
+function mostrarCambiarContraseña() {
+    if (contraseñaFormVisible) {
+        document.getElementById("cambiarContraseñaForm").style.display = "none";
+        contraseñaFormVisible = false;
+    } else {
+        document.getElementById("cambiarContraseñaForm").style.display = "block";
         document.getElementById("editarFotoForm").style.display = "none";
         document.getElementById("editarDatosForm").style.display = "none";
-        document.getElementById("cambiarContraseñaForm").style.display = "block";
+        contraseñaFormVisible = true;
+        fotoFormVisible = false;
+        datosFormVisible = false;
     }
+}
 
-    document.getElementById("avatar").onchange = function (e) {
-        var reader = new FileReader();
-        reader.onload = function (event) {
-            document.getElementById("preview").src = event.target.result;
-        }
-        reader.readAsDataURL(e.target.files[0]);
-    };
+document.getElementById("avatar").onchange = function (e) {
+    var reader = new FileReader();
+    reader.onload = function (event) {
+        document.getElementById("preview").src = event.target.result;
+    }
+    reader.readAsDataURL(e.target.files[0]);
+};
+
